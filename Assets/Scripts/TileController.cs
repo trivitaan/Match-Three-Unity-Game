56df87a834cb6a976ffb809c9e45ae3d11b5e49d
Trivita Annisa Utami <trivita1900018074@webmail.uad.ac.id>
2021-09-25 00:56:02 +0700

    match tiles

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TileController : MonoBehaviour
 {
@@ -17,6 +18,102 @@ public class TileController : MonoBehaviour
 
     private bool isSelected = false;
 
+    private static readonly Vector2[] adjacentDirection = new Vector2[]{Vector2.up, Vector2.down, Vector2.left, Vector2.right};
+
+    #region Adjacent
+
+    #region Check Match
+    
+    private List<TileController> GetMatch(Vector2 castDir)
+    {
+        List<TileController> matchingTiles = new List<TileController>();
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir, render.size.x);
+
+        while(hit)
+        {
+            TileController otherTile = hit.collider.GetComponent<TileController>();
+            if(otherTile.id != id || otherTile.IsDestroyed)
+            {
+                break;
+            }
+
+            matchingTiles.Add(otherTile);
+            hit = Physics2D.Raycast(otherTile.transform.position, castDir, render.size.x);
+        }
+        return matchingTiles;
+    }
+
+    private List<TileController> GetOneLineMatch(Vector2[] paths)
+    {
+        List<TileController> matchingTiles = new List<TileController>();
+        for(int i = 0; i < paths.Length; i++)
+        {
+            matchingTiles.AddRange(GetMatch(paths[i]));
+        }
+        //only match when more than 2 (3 with itself) in one line
+        if(matchingTiles.Count >= 2)
+        {
+            return matchingTiles;
+        }
+        return null;
+    }
+
+    public List<TileController> GetAllMatches()
+    {
+        if(IsDestroyed)
+        {
+            return null;
+        }
+
+        List<TileController> matchingTiles = new List<TileController>();
+        //get matches for horizontal and vertical
+        List<TileController> horizontalMatchingTiles = GetOneLineMatch(new Vector2[2]{Vector2.up, Vector2.down});
+        List<TileController> verticalMatchingTiles = GetOneLineMatch(new Vector2[2]{Vector2.left, Vector2.right});
+
+        if(horizontalMatchingTiles != null)
+        {
+            matchingTiles.AddRange(horizontalMatchingTiles);
+        }
+        if(verticalMatchingTiles != null)
+        {
+            matchingTiles.AddRange(verticalMatchingTiles);
+        }
+
+        //add itself to matched tiles if match found
+        if(matchingTiles!= null && matchingTiles.Count >= 2)
+        {
+            matchingTiles.Add(this);
+        }
+        return matchingTiles;
+    }
+
+    #endregion
+
+
+    private TileController GetAdjacent(Vector2 castDir)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir, render.size.x);
+        if (hit)
+        {
+            return hit.collider.GetComponent<TileController>();
+        }
+
+        return null;
+    }
+
+    public List<TileController> GetAdjacentTiles()
+    {
+        List<TileController> adjacentTiles = new List<TileController>();
+        for(int i = 0; i < adjacentDirection.Length; i++)
+        {
+            adjacentTiles.Add(GetAdjacent(adjacentDirection[i]));
+        }
+
+        return adjacentTiles;
+    }
+
+    #endregion
+
     private void Awake()
     {
         board = BoardManager.Instance;
@@ -26,7 +123,7 @@ public class TileController : MonoBehaviour
     private void OnMouseDown()
     {
         //Non Selectable conditions
-        if(render.sprite == null)
+        if(render.sprite == null || board.IsAnimating )
         {
             return;
         }
@@ -45,19 +142,49 @@ public class TileController : MonoBehaviour
             }
             else
             {
-                previousSelected.Deselect();
-                Select();
+                //is this adjacent tile?
+                if(GetAdjacentTiles().Contains(previousSelected))
+                {
+                    TileController otherTile = previousSelected;
+                    previousSelected.Deselect();
+
+                    //swap tile
+                    SwapTile(otherTile, ()=>{SwapTile(otherTile);});
+                }
+                else
+                {
+                    //if not adjacent then change selected
+                    previousSelected.Deselect();
+                    Select();
+                }
             }
         }
     }
 
-    public IEnumerator MoveTilePosition(Vector2 targetPosition, System.ActiononCompleted)
+    public void SwapTile(TileController otherTile, System.Action onCompleted = null)
+    {
+        StartCoroutine(board.SwapTilePosition(this, otherTile, onCompleted));
+    }
+
+    public IEnumerator MoveTilePosition(Vector2 targetPosition, System.Action onCompleted)
     {
         Vector2 startPosition = transform.position;
         float time = 0.0f;
 
         //run animation on next frame for safety reasons
         yield return new WaitForEndOfFrame();
+
+        while(time < moveDuration)
+        {
+            transform.position = Vector2.Lerp(startPosition, targetPosition, time/moveDuration);
+            time += Time.deltaTime;
+
+            yield return new WaitForEndOfFrame();
+        }
+
+        transform.position = targetPosition;
+
+        onCompleted?.Invoke();
     }
 
     #region Select and Deselect
